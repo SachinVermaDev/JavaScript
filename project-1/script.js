4a9145b36900f2df2baa02712b05e67951a60e88
Sachin Verma <sachinverma.info.100@gmail.com>
2023-12-05 00:23:15 +0530

    5 Dec Commit

--- project-1/script.js
+++ project-1/script.js
@@ -1,3 +1,12 @@
-var a;
-a=5;
-alert(a);
+let x;
+let z='y';
+let a='yes';
+let b='n';
+let c='no';
+if(x==1||x==z|| x==a){
+    console.log("continue...");
+}else if(x==0||x==b|| x==c){
+console.log("End...");
+}else{
+    console.log("Maa chudane do duniya-dari ki");
+}
