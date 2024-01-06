1b71ecbb25140d9152be6a81aded4b1647cdc143
Sachin Verma <sachinverma.info.100@gmail.com>
2024-01-07 01:03:59 +0530

    commited

--- practice/script.js
+++ practice/script.js
@@ -1,5 +1,3 @@
-
-let elm=document.getElementsByTagName("h2");
-for(let i=0; i<=elm.length; i++){
-    elm[i].innerHTML= "hello";
+function myFunction(){
+    document.getElementById('demo').innerHTML="Paragraph Changed.";
 }
